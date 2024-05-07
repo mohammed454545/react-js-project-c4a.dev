17febfac1f24fc5181ac794820564a3a17b18d28
mohammed454545 <mohammed.h.salh@gmail.com>
2024-05-07 11:52:08 +0300

    edit app.js and index.css

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@
 function App() {
   return (
     <>
-      <h1 className="title">c4a.dev</h1>
-      <h2>mohammed</h2>
+    <h1 className="title">version 2 </h1>
+      <h2 >c4a.dev</h2>
+      <h3>mohammed</h3>
     </>
   );
 }
